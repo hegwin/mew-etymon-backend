4e3f3557eb44feefd09303d330c66a430dbe2aec
hegwin <zwt315@163.com>
2018-02-27 16:08:45 +0800

    Update root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :words
   resources :etymons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  root 'etymons#index'
 end

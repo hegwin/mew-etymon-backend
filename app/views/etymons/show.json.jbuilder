00a0905a2e150798494813dbf4a87eb787cb28b6
hegwin <zwt315@163.com>
2018-03-01 12:33:56 +0800

    [#154988535] Remove useless actions and views for Etymon

--- app/views/etymons/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.partial! "etymons/etymon", etymon: @etymon
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   post "/graphql", to: "graphql#execute"
-  resources :words
+  resources :words, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   root 'etymons#index'

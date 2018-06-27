302704d8d913d8d22e112f00335871638704aa1e
hegwin <zwt315@163.com>
2018-06-27 12:27:52 +0800

    [#158518836] Add language admin

--- app/admin/languages.rb
+++ app/admin/languages.rb
@@ -1,15 +1,37 @@
 ActiveAdmin.register Language do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  menu priority: 3
 
+  permit_params :parent_id, :name, :name_cn, :period, :description
+
+  controller do
+    helper ActionView::Helpers::TextHelper
+
+    def scoped_collection
+      super.includes :parent
+    end
+  end
+
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :name_cn
+    column :period
+    column :parent
+    column :description do |language|
+      language.description.present? ? language.description.first(20) + '...'  : 'N/A'
+    end
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :parent
+      f.input :name
+      f.input :name_cn, label: 'Name CN'
+      f.input :period
+      f.input :description, input_html: { rows: 6 }
+    end
+    f.actions
+  end
 end
--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,5 @@
 class Language < ApplicationRecord
   acts_as_nested_set
+
+  validates :name, presence: true, uniqueness: true
 end

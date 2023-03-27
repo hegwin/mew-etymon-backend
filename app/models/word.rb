ee758ca69b874069d2c76d4434be1366c817c87a
Hegwin <zwt315@163.com>
2023-03-27 15:02:55 +0800

    [#184797747] Query word meaning from after_create to after_save

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,7 @@
 class Word < ApplicationRecord
   has_and_belongs_to_many :etymons
 
-  after_create :query_word_meaning, unless: :meaning
+  after_save :query_word_meaning, unless: :meaning
 
   def etymons_string
     etymons.map(&:spelling).join(',')
@@ -11,7 +11,7 @@ class Word < ApplicationRecord
     meaning = dict.query(spelling&.strip)
     return nil if meaning.nil?
 
-    update_attributes meaning: meaning
+    update meaning: meaning
   end
 
   private
